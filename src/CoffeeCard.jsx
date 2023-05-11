cae12b36f65502a0ca6a0a868b791dcaa6f68956
rakibsarowar <rakibsarowar@gmail.com>
2023-05-11 22:29:54 +0600

    added code for deleted item from db

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,5 +1,45 @@
+import Swal from 'sweetalert2'
+
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    // Delete Operattion -------------------------------------
+    const handleDelete = (_id) => {
+        console.log(_id)
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire(
+                //     'Deleted!',
+                //     'Your file has been deleted.',
+                //     'success'
+                // )
+                fetch(`http://localhost:5000/coffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
     return (
         <div >
             <div className="card card-side bg-base-100 shadow-xl m-20">
@@ -8,14 +48,16 @@ const CoffeeCard = ({ coffee }) => {
                 </div>
                 <div className="flex p-4 gap-4">
                     <div>
-                    <h2 className="card-title">{name}</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
+                        <h2 className="card-title">{name}</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
                     </div>
                     <div className="card-actions justify-end">
                         <div className="btn-group btn-group-vertical space-y-4">
                             <button className="btn btn-active">View</button>
                             <button className="btn">Edit</button>
-                            <button className="btn">X</button>
+                            <button
+                                onClick={() => handleDelete(_id)}
+                                className="btn bg-orange-500">X</button>
                         </div>
                     </div>
                 </div>
