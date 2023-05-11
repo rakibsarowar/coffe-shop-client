d4c5051af4e1f292eab1ec3c74dcfcba54dc1d10
rakibsarowar <rakibsarowar@gmail.com>
2023-05-11 21:41:24 +0600

    showing data from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const coffees = useLoaderData();
+
   return (
     <>
-      <h1 className='text-6xl'>Vite + React</h1>
+      <h1 className='text-6xl'>Hot Cofee: {coffees.length}</h1>
     </>
   )
 }
